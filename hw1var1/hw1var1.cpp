8a7f4e450aa80bc5c0dcc93a70aa27148e8c5914
Kiiiiisaaa <114130300+Kiiiiisaaa@users.noreply.github.com>
2022-12-14 01:13:27 +0300

    Add files via upload

---
+++ hw1var1/hw1var1.cpp
@@ -0,0 +1,12 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+int main() {
+	float x1 = 0, y1 = 3, x2 = 4, y2 = 0, x3 = 0, y3 = 0, p, s, a, b, c;
+	a = sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+	b = sqrt((x3 - x2) * (x3 - x2) + (y3 - y2) * (y3 - y2));
+	c = sqrt((x1 - x3) * (x1 - x3) + (y1 - y3) * (y1 - y3));
+	p = (a + b + c) / 2;
+	s = sqrt(p * (p - a) * (p - b) * (p - c));
+	printf("P = %f\nS = %f", p * 2, s);
+}
---
+++ hw1var2/hw1var2.cpp
@@ -0,0 +1,7 @@
+﻿#include <stdio.h>
+
+int main() {
+	int i;
+	scanf_s("%i", &i);
+	printf("1: %i\n2: %i\n3: %i", i + 1, i + 2, i + 3);
+}
